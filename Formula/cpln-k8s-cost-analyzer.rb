c204b0f569daa4b0450d743e75e2364308a609a8
MajidAbuRmila <majid@controlplane.com>
2023-08-03 16:05:33 +0300

    Update cpln-k8s-cost-analyzer to version 1.0.4

--- Formula/cpln-k8s-cost-analyzer.rb
+++ Formula/cpln-k8s-cost-analyzer.rb
@@ -1,9 +1,9 @@
 class CplnK8sCostAnalyzer < Formula
   desc "Control Plane K8s Cost Analyzer"
   homepage "https://controlplane.com"
-  url "https://github.com/controlplane-com/k8s-cost-analyzer/archive/refs/tags/v1.0.3.zip"
-  sha256 "f12df85e7161b878168d20059c2d07e57ece0b1dd7c318fc94356aae0d052ac4"
-  version "1.0.3"
+  url "https://github.com/controlplane-com/k8s-cost-analyzer/archive/refs/tags/v1.0.4.zip"
+  sha256 "7490e2b8212e3248a188543b3ef047ccd0b895925c58c8d3652ed9f97795130f"
+  version "1.0.4"
   license "GPL-3.0-only"
 
   depends_on "python"
@@ -22,6 +22,6 @@ class CplnK8sCostAnalyzer < Formula
 
   test do
     # Run the binary with the "--version" flag
-    assert_match "1.0.3", shell_output("#{bin}/cpln-k8s-cost-analyzer --version")
+    assert_match "1.0.4", shell_output("#{bin}/cpln-k8s-cost-analyzer --version")
   end
 end
