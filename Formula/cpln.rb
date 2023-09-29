7d4dd30bfe5109bcfb480eaeec40abcb81d7e21c
MajidAbuRmila <majid@controlplane.com>
2023-09-29 16:07:16 +0300

    Update cpln to version 1.2.4

--- Formula/cpln.rb
+++ Formula/cpln.rb
@@ -2,13 +2,13 @@ class Cpln < Formula
   desc "Control Plane CLI"
   homepage "https://controlplane.com"
   if OS.mac?
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/925739349-9784f19a/cpln-macos.tgz",
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1017862696-b76e2454/cpln-macos.tgz",
         verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
-    sha256 "ff82e75e1f9ee7fb265b7e33439fec83193432ee54c35da03db04de40ecaa3d1"
+    sha256 "1005a3aec01590706c86c3377373c517599673c91f7244e9bd4d0608278185e6"
   else
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/925739349-9784f19a/cpln-linux.tgz",
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1017862696-b76e2454/cpln-linux.tgz",
         verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
-    sha256 "4002d8eca7cc856386ee082a019ff2b401c84e0ef8938e34c0c67aff1e10b026"
+    sha256 "d6e0c66e97925c836e1f54fb6fc50ecbc6c84b5fb3b082d42779101392958cc8"
   end
   version "1.1.0"
   license "GPL-3.0-only"
