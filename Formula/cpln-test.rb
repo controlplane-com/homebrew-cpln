71af1dc75ceb5cfa27616754a748a66533394f98
MajidAbuRmila <majid@controlplane.com>
2023-10-27 11:15:35 +0000

    Update cpln to version 1.3.1

---
+++ Formula/cpln-test.rb
@@ -0,0 +1,42 @@
+class Cpln < Formula
+  desc "Control Plane CLI"
+  homepage "https://controlplane.com"
+  if OS.mac?
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/binaries/cpln/1051818513-f4fc9226/cpln-macos.tgz",
+        verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
+    sha256 "4ce33934e2c55611b18b1de2ccab91c0046a005aafec5c645a8d4a34c5e7b87d"
+  else
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/binaries/cpln/1051818513-f4fc9226/cpln-linux.tgz",
+        verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
+    sha256 "b712e0902baa8b5cf408faad570e051dffc367d16d196b3df0f7151731c22660"
+  end
+  version "1.3.1"
+  license "GPL-3.0-only"
+
+  def install
+    bin.install "cpln"
+    bin.install "docker-credential-cpln"
+    # `docker-credential-cpln` is a tool required by the CLI allowing Docker
+    # to authenticate to your org's private image registry in Control Plane.
+  end
+
+  test do
+    # Run the CLI binary with the "--version" flag
+    assert_match "1.3.1", shell_output("#{bin}/cpln --version")
+
+    # Run the CLI binary with the "--help" flag
+    assert_match "Control Plane Corporation", shell_output("#{bin}/cpln --help")
+
+    # Attempt to create a CPLN profile
+    test_profile = "test-profile"
+    test_org = "test-org"
+    test_gvc = "test-gvc"
+
+    system "#{bin}/cpln", "profile", "create", test_profile, "--org", test_org, "--gvc", test_gvc
+    assert_match test_profile, shell_output("#{bin}/cpln profile get")
+
+    # Set profile as default
+    system "#{bin}/cpln", "profile", "set-default", test_profile
+    assert_match "*", shell_output("#{bin}/cpln profile get")
+  end
+end
