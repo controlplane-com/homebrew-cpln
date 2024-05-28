6468075279afccf291515297fbadce1c4a6a0237
Majid Abu Rmila <120715558+MajidAbuRmila@users.noreply.github.com>
2024-05-28 17:30:40 +0300

    Update cpln.rb

--- Formula/cpln.rb
+++ Formula/cpln.rb
@@ -1,15 +1,16 @@
 class Cpln < Formula
   desc "Control Plane CLI"
   homepage "https://controlplane.com"
-  if Hardware::CPU.arm?
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-arm64.dmg",
-        verified: "storage.googleapis.com"
-    sha256 "026d07f073417d5280d3ecb3675f90955312d1e293fb4b5fdc4f6ddd6ebd6e77"
-  else
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-x64.dmg",
-        verified: "storage.googleapis.com"
-    sha256 "a2dd42d8802fbf78a1aebc78a91f5d697e94107fbbc4ad6bb7aeb6836ce7b0c8"
-  end
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-arm64.dmg",
+          verified: "storage.googleapis.com"
+      sha256 "026d07f073417d5280d3ecb3675f90955312d1e293fb4b5fdc4f6ddd6ebd6e77"
+    else
+      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-x64.dmg",
+          verified: "storage.googleapis.com"
+      sha256 "a2dd42d8802fbf78a1aebc78a91f5d697e94107fbbc4ad6bb7aeb6836ce7b0c8"
+    end
   else
     url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-linux.tgz",
         verified: "storage.googleapis.com"
