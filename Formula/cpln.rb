d7c61c72892648cfe5633cf4c992853b46e66990
MajidAbuRmila <majid@controlplane.com>
2024-05-28 17:28:37 +0300

    Update cpln to version 2.2.2

--- Formula/cpln.rb
+++ Formula/cpln.rb
@@ -1,28 +1,56 @@
 class Cpln < Formula
   desc "Control Plane CLI"
   homepage "https://controlplane.com"
-  if OS.mac?
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1296536730-d1ddd208/cpln-macos.tgz",
-        verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
-    sha256 "acf1b12329c7698f633cfc16ece698e607f6a35cf63946b76dd3c9f2e4de80d4"
+  if Hardware::CPU.arm?
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-arm64.dmg",
+        verified: "storage.googleapis.com"
+    sha256 "026d07f073417d5280d3ecb3675f90955312d1e293fb4b5fdc4f6ddd6ebd6e77"
   else
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1296536730-d1ddd208/cpln-linux.tgz",
-        verified: "https://storage.googleapis.com/artifacts.cpln-build.appspot.com"
-    sha256 "1d66d1b884f6db80f704ccc5eacf478257dc593aa471c8036f0629e9ae2209fd"
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-macos-x64.dmg",
+        verified: "storage.googleapis.com"
+    sha256 "a2dd42d8802fbf78a1aebc78a91f5d697e94107fbbc4ad6bb7aeb6836ce7b0c8"
   end
-  version "2.2.1"
+  else
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1308697044-753de269/cpln-linux.tgz",
+        verified: "storage.googleapis.com"
+    sha256 "1ae9bd39240cea741bb68b324588b5b1f7afd2eaa4b38eb975862827dfa03903"
+  end
+  version "2.2.2"
   license "GPL-3.0-only"
 
   def install
-    bin.install "cpln"
-    bin.install "docker-credential-cpln"
+    if OS.mac?
+      dmg_mountpoint = "#{buildpath}/dmg"
+      staging_dir = "#{buildpath}/staging"
+
+      # Create a directory to stage the files
+      mkdir staging_dir
+
+      # Mount the DMG file
+      system "hdiutil", "attach", cached_download, "-mountpoint", dmg_mountpoint
+      
+      # Copy the files from the DMG to the staging directory
+      cp "#{dmg_mountpoint}/cpln", staging_dir
+      cp "#{dmg_mountpoint}/docker-credential-cpln", staging_dir
+
+      # Unmount the DMG file
+      system "hdiutil", "detach", dmg_mountpoint
+
+      # Install the files from the staging directory
+      bin.install "#{staging_dir}/cpln"
+      bin.install "#{staging_dir}/docker-credential-cpln"
+    else
+      bin.install "cpln"
+      bin.install "docker-credential-cpln"
+    end
+
     # `docker-credential-cpln` is a tool required by the CLI allowing Docker
     # to authenticate to your org's private image registry in Control Plane.
   end
 
   test do
     # Run the CLI binary with the "--version" flag
-    assert_match "2.2.1", shell_output("#{bin}/cpln --version")
+    assert_match "2.2.2", shell_output("#{bin}/cpln --version")
 
     # Run the CLI binary with the "--help" flag
     assert_match "Control Plane Corporation", shell_output("#{bin}/cpln --help")
