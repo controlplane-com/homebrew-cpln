870578c33eeb13f156487f956467d75696691287
MajidAbuRmila <majid@controlplane.com>
2025-01-21 03:40:57 +0200

    Update cpln-k8s-cost-analyzer to version 1.1.3

--- Formula/cpln-k8s-cost-analyzer.rb
+++ Formula/cpln-k8s-cost-analyzer.rb
@@ -2,13 +2,13 @@ class CplnK8sCostAnalyzer < Formula
   desc "Control Plane K8s Cost Analyzer"
   homepage "https://controlplane.com"
   if OS.mac?
-    url "https://github.com/controlplane-com/k8s-cost-analyzer/releases/download/v1.0.5/k8s-cost-analyzer-macOS"
-    sha256 "4c65492fa59d1451baba4842982c58438b9a51f282f20e9a27010f97d7d94db2"
+    url "https://github.com/controlplane-com/k8s-cost-analyzer/releases/download/v1.1.3/k8s-cost-analyzer-macOS-arm64"
+    sha256 "53d6fda6fa23060396a1c1da4932fa82816a8a6c83718a3ad4851b8cce0cfe66"
   else
-    url "https://github.com/controlplane-com/k8s-cost-analyzer/releases/download/v1.0.5/k8s-cost-analyzer-linux"
-    sha256 "87af3a9c7919a89e5d16a87f715e02bcef6b34faea0507c8634e001dec3e12d8"
+    url "https://github.com/controlplane-com/k8s-cost-analyzer/releases/download/v1.1.3/k8s-cost-analyzer-linux"
+    sha256 "2febb195d951220605eb1e56bc8828461407f88d9c0962a0b149759a81748c8e"
   end
-  version "1.0.5"
+  version "1.1.3"
   license "GPL-3.0-only"
   
   def install
@@ -21,6 +21,6 @@ class CplnK8sCostAnalyzer < Formula
 
   test do
     # Run the binary with the "--version" flag
-    assert_match "1.0.5", shell_output("#{bin}/cpln-k8s-cost-analyzer --version")
+    assert_match "1.1.3", shell_output("#{bin}/cpln-k8s-cost-analyzer --version")
   end
 end
