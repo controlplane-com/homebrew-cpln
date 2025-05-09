bcf3c8b5cca0934921568aca6f74569b7f4678f2
MajidAbuRmila <majid@controlplane.com>
2025-05-09 20:20:31 +0300

    Update cpln to version 3.5.2

--- Formula/cpln.rb
+++ Formula/cpln.rb
@@ -3,20 +3,20 @@ class Cpln < Formula
   homepage "https://controlplane.com"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1801013340-8dfda1b2/cpln-macos-arm64.dmg",
+      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1809643413-4654d3d2/cpln-macos-arm64.dmg",
           verified: "storage.googleapis.com"
-      sha256 "40c80f1cb3714a0b7be5fbe923fe9772dfab89c82222f9b6d3200344903793ae"
+      sha256 "38841636c4dd701f6baa5b7537f76a8ffbaabf59fa5888a7c50e0a12a7d4ded6"
     else
-      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1801013340-8dfda1b2/cpln-macos-x64.dmg",
+      url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1809643413-4654d3d2/cpln-macos-x64.dmg",
           verified: "storage.googleapis.com"
-      sha256 "9d58af69a840745aa317d798235419270d26ded0d4b73ed579131ae058a0ef8a"
+      sha256 "4e4f130d60475e8c034ba007df71ab728ff14b22259caec492e7fabe7c435263"
     end
   else
-    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1801013340-8dfda1b2/cpln-linux.tgz",
+    url "https://storage.googleapis.com/artifacts.cpln-build.appspot.com/binaries/cpln/1809643413-4654d3d2/cpln-linux.tgz",
         verified: "storage.googleapis.com"
-    sha256 "11cab974809ff22003bcfc6c86d50bcabf5e0c217cbc831df3ce186c246fb042"
+    sha256 "f4e851be18a349f1d10d829eddaca7a0e706ed7ff6cd0a8c4189b2fa11137ae4"
   end
-  version "3.5.1"
+  version "3.5.2"
   license "GPL-3.0-only"
 
   def install
@@ -51,7 +51,7 @@ class Cpln < Formula
 
   test do
     # Run the CLI binary with the "--version" flag
-    assert_match "3.5.1", shell_output("#{bin}/cpln --version")
+    assert_match "3.5.2", shell_output("#{bin}/cpln --version")
 
     # Run the CLI binary with the "--help" flag
     assert_match "Control Plane Corporation", shell_output("#{bin}/cpln --help")
